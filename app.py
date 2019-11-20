9b504cb628f802c56f5f8996797fc811d224efb4
Mark Ritchie <mwritchie2@gmail.com>
2019-11-20 17:01:56 -0600

    laid groundwork for models

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, jsonify, g
 import os
+import models
 
 app = Flask('media-api')
 app.secret_key = os.environ.get(
     'SECRET_KEY', 'notreallyasecretbutsureyoudoyoudude'
 )
 
+
+@app.before_request
+def before_request():
+    print("connected to db")
+    g.db = models.DATABASE
+    g.db.connect()
+
+
+@app.after_request
+def after_request(res):
+    print("disconnected from db")
+    g.db.close()
+    return res
+
+
 if __name__ == '__main__':
+    models.initialize()
     app.run(
         debug=os.environ.get('DEBUG', True),
         port=os.environ.get('PORT', 8000)
---
+++ models.py
@@ -0,0 +1,55 @@
+from peewee import *
+from flask_login import UserMixin
+
+DATABASE = SqliteDatabase('media.sqlite')
+
+
+class User(UserMixin, Model):
+    username = CharField(64, unique=True)
+    email = CharField(64, unique=True)
+    password = CharField()
+
+    class Meta:
+        database = DATABASE
+
+
+class Media(Model):
+    title = CharField()
+    year_of_release = IntegerField(
+        constraints=[
+            Check('year_of_release > 1905'),
+            Check('year_of_release < 2020')
+        ]
+    )
+    genre = CharField(32)
+    poster_url = CharField()
+    external_id = IntegerField(unique=True)
+
+    class Meta:
+        database = DATABASE
+
+
+class Viewership(Model):
+    user_id = ForeignKeyField(User)
+    media_id = ForeignKeyField(Media)
+
+    class Meta:
+        database = DATABASE
+
+
+class Review(Model):
+    rating = IntegerField(constraints=[Check('rating < 6')])
+    body = TextField()
+    user_id = ForeignKeyField(User)
+    media_id = ForeignKeyField(Media)
+    date_added = TimestampField()
+
+    class Meta:
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Media, Viewership, Review], safe=True)
+    print("Tables Flipped")
+    DATABASE.close()
