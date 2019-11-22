e42c8c605af061648b78886dadec06088d244be6
Mark Ritchie <mwritchie2@gmail.com>
2019-11-22 12:51:53 -0600

    enabled updating reviews

--- resources/reviews.py
+++ resources/reviews.py
@@ -29,3 +29,39 @@ def post_review():
                 "message": "No media matches the provided media_id."
             }
         ), 422
+
+
+@review.route('/<revId>', methods=['PUT'])
+@login_required
+def update_review(revId):
+    payload = request.get_json()
+    print(payload)
+    try:
+        review = models.Review.get_by_id(revId)
+        if model_to_dict(review.user_id)['id'] == current_user.id:
+            review.rating = payload['rating'] if payload['rating'] else None
+            review.body = payload['body'] if payload['body'] else None
+            review.save()
+            return jsonify(
+                data=model_to_dict(review),
+                status={
+                    "code": 200,
+                    "message": "Review successfully updated."
+                }
+            )
+    except models.DoesNotExist:
+        return jsonify(
+            data={},
+            status={
+                "code": 422,
+                "message": "No review matches the provided media_id."
+            }
+        ), 422
+    else:
+        return jsonify(
+            data={},
+            status={
+                "code": 403,
+                "message": "That's not your review."
+            }
+        ), 403
